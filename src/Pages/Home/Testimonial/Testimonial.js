901097c7ea06a67f2f275d7fcd9145f1a3f57e50
wahid30 <md.wahidulalamjp2@gmail.com>
2023-05-31 01:51:14 +0600

    [done]

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -8,14 +8,14 @@ const Testimonial = () => {
       {/* person 01  */}
       <div className="flex items-start justify-center gap-5 my-5">
         <div>
-          <div className="avatar">
+          {/* <div className="avatar">
             <div className="w-24 rounded-full">
               <img src="https://th.bing.com/th/id/R.c82461205d4218eede05a44b5b1c643d?rik=81LZoxzibmEzZg&pid=ImgRaw&r=0" />
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="grid gap-2">
-          <h1 className="font-bold">Niloy</h1>
+          <h1 className="font-bold">Tanvirul Islam Niloy</h1>
           <p className="text-sm">I'm using react on that project and handle the backend by using node.js and mongodb</p>
         </div>
       </div>
@@ -23,28 +23,28 @@ const Testimonial = () => {
       <div className="flex items-start justify-center gap-5 my-5">
         <div>
           <div className="avatar">
-            <div className="w-24 rounded-full">
+            {/* <div className="w-24 rounded-full">
               <img src="https://th.bing.com/th/id/R.c82461205d4218eede05a44b5b1c643d?rik=81LZoxzibmEzZg&pid=ImgRaw&r=0" />
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="grid gap-2">
-          <h1 className="font-bold">Niloy</h1>
-          <p className="text-sm">I'm using react on that project and handle the backend by using node.js and mongodb</p>
+          <h1 className="font-bold">Tajbirul Jannat Mili</h1>
+          <p className="text-sm">I'm using the google authentication for the registration system in our project.</p>
         </div>
       </div>
       {/* person 03  */}
       <div className="flex items-start justify-center gap-5 my-5">
         <div>
           <div className="avatar">
-            <div className="w-24 rounded-full">
+            {/* <div className="w-24 rounded-full">
               <img src="https://th.bing.com/th/id/R.c82461205d4218eede05a44b5b1c643d?rik=81LZoxzibmEzZg&pid=ImgRaw&r=0" />
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="grid gap-2">
-          <h1 className="font-bold">Niloy</h1>
-          <p className="text-sm">I'm using react on that project and handle the backend by using node.js and mongodb</p>
+          <h1 className="font-bold">Asura Khanom Mahi</h1>
+          <p className="text-sm">I'm using react and tailwing css for the front-end design.</p>
         </div>
       </div></div>
     </div>
